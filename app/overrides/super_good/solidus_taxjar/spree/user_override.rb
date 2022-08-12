58ab2aa884e1d0c41f70012bf1c935b3d9781450
Adnan Abdulally <adnan.abdulally@printivity.com>
2022-08-12 10:29:07 -0700

    add taxjar_customer association to user

---
+++ app/overrides/super_good/solidus_taxjar/spree/user_override.rb
@@ -0,0 +1,16 @@
+module SuperGood
+  module SolidusTaxjar
+    module Spree
+      module UserOverride
+        def self.prepended(base)
+          base.has_one :taxjar_customer,
+                       class_name: "SuperGood::SolidusTaxjar::Customer",
+                       dependent: :destroy,
+                       inverse_of: :user
+        end
+
+        ::Spree::UserClassHandle.new.prepend self
+      end
+    end
+  end
+end

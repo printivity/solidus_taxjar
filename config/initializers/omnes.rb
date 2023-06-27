4e7f7ae69df8f6f719e380f85ded72295052b0fc
Adnan Abdulally <adnan.abdulally@printivity.com>
2023-06-27 11:49:24 -0700

    add a couple missing events

--- config/initializers/omnes.rb
+++ config/initializers/omnes.rb
@@ -4,6 +4,8 @@ Rails.application.config.to_prepare do
     tax_exemption_created
     tax_exemption_updated
     tax_exemption_destroyed
+    tax_exemption_approved
+    tax_exemption_disapproved
     tax_exemption_customer_request
   ].each { |event_name| Spree::Bus.register(event_name) }
   SuperGood::SolidusTaxjar::Spree::ReportingSubscriber.new.subscribe_to(Spree::Bus)

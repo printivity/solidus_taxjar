35defe67ddb3ad9988cd02b33a9842b71acd35a3
Adnan Abdulally <adnan.abdulally@printivity.com>
2023-07-18 14:55:08 -0700

    fix backfill transaction sync again

--- app/jobs/super_good/solidus_taxjar/backfill_transaction_sync_batch_job.rb
+++ app/jobs/super_good/solidus_taxjar/backfill_transaction_sync_batch_job.rb
@@ -6,16 +6,17 @@ module SuperGood
       queue_as { SuperGood::SolidusTaxjar.job_queue }
 
       def perform(transaction_sync_batch)
-        complete_orders = ::Spree::Order.complete.where(shipment_state: 'shipped').select do |o|
-          SuperGood::SolidusTaxjar.reportable_order_check.call(o)
-        end
+        complete_orders = ::Spree::Order.complete.where(shipment_state: 'shipped')
         if transaction_sync_batch.start_date
           complete_orders = complete_orders.where("completed_at >= ?", transaction_sync_batch.start_date.beginning_of_day)
         end
         if transaction_sync_batch.end_date
           complete_orders = complete_orders.where("completed_at <= ?", transaction_sync_batch.end_date.end_of_day)
         end
-        complete_orders.find_each do |order|
+        complete_orders = complete_orders.select do |o|
+          SuperGood::SolidusTaxjar.reportable_order_check.call(o)
+        end
+        complete_orders.each do |order|
           next if order.taxjar_order_transactions.any?
           SuperGood::SolidusTaxjar::ReportTransactionJob.perform_later(order, transaction_sync_batch)
         end
